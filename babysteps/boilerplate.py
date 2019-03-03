c7ee212370fc92c05255b887b7fc0ff98e056620
Mark Voltz <mark.voltz@gmail.com>
2019-03-03 11:19:31 -0600

    deleting files without numbers in front of the name.

--- babysteps/boilerplate.py
+++
@@ -1,2 +0,0 @@
-print("Read the source code to better understand what's going on. \
-The output is usually rubbish.")
--- babysteps/multiline_string.py
+++
@@ -1,5 +0,0 @@
-long_string = '''It was the best of times.
-It was the worst of time.
-This was the summer of our discontent.'''
-
-print(long_string)
