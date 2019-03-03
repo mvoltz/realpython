89e81bd502921d5c2d36a3a4cc55cdbfa12f728c
Mark Voltz <mark.voltz@gmail.com>
2019-03-03 09:08:57 -0600

    added multiline_strings.py.

---
+++ babysteps/multiline_string.py
@@ -0,0 +1,5 @@
+long_string = '''It was the best of times.
+It was the worst of time.
+This was the summer of our discontent.'''
+
+print(long_string)
---
+++ babysteps/multiline_strings.py
@@ -0,0 +1,31 @@
+# span multiple lines
+
+long_string = '''It was the best of times.
+It was the worst of time.
+This was the summer of our discontent.'''
+
+print(long_string)
+
+print("\n")
+
+# span and preserve white space
+
+print('''the quick brown
+     fox jumps over a
+         lazy dog.''')
+
+'''
+for ease of coding, use a backslash
+to keep strings readable, without
+forcing the string output to break.
+'''
+
+print("\n")
+
+mystring = 'this string appears \
+to span multiple lines \
+in the source code, but \
+prints as a single string \
+with no breaks.'
+
+print(mystring)
